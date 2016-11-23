ed5c08feff6e000efbc202bab06809d6015223c0
Zen Shen <zen.shen0302@gmail.com>
2016-11-23 15:28:16 +0800

    facebook key and secret naming

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FBOOK_KEY'], ENV['FBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
